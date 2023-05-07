ca5571067eb1a6f393d317f247e247cc7613537f
Carl Lyons <code@carledwardlyons.ca>
2023-05-07 13:54:58 -0400

    Initialize Package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'quick-rest',
+    version = '0.0.1',
+    description = 'A lightweight toolset for structuring API applications.',
+    packages = ['quickrest'],
+    package_dir = {'' : 'src'},
+    install_requires = [
+        'Django>=4.2',
+    ]
+)
